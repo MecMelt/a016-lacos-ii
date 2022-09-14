28c1b83318a997d244da94be3076b950e806dcac
MecMelt <eltonlimeira10@gmail.com>
2022-09-13 23:50:58 -0300

    exerciciolacos2

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+//Crie um programa que peça um input de número para o usuário. 
+//Com este número, o código deve imprimir a **tabuada** do número, de 1 a 10.
+
+let num = Number(prompt("Digite um número e descubra sua tabuáda"))
+const array = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+// for (let i in array){
+//     i++
+//     console.log(`Aqui está : ${num * array[i]}`)
+// }
+
+for ( let i in array){
+    console.log(`${num} * ${array[i]} = ${num * array[i]}`)
+}
